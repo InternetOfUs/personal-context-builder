cd433cb815e85e7cf9711f538730b82caa376dc2
William Droz <william.droz@idiap.ch>
2019-01-30 16:58:04 +0100

    sanic template

---
+++ sanic_app.py
@@ -0,0 +1,18 @@
+from sanic import Sanic
+from sanic.views import HTTPMethodView
+from sanic.response import text
+from sanic.response import json
+
+app = Sanic("wenet_personal_context_builder")
+
+
+class SimpleView(HTTPMethodView):
+    def post(self, request):
+        return json({"stay": "points"})
+
+
+app.add_route(SimpleView.as_view(), "/staypoints/")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
