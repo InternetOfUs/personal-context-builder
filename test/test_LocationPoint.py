ccac5a798771a51b3e435e98093418fc669c35a0
William DROZ <william.droz@idiap.ch>
2019-01-31 09:10:00 +0100

    test add operator

--- test/test_LocationPoint.py
+++ test/test_LocationPoint.py
@@ -25,6 +25,14 @@ class LocationPointTestCase(unittest.TestCase):
         distance_truth = 98922.59
         self.assertAlmostEqual(distance, distance_truth, delta=2)
 
+    def test_add(self):
+        t1 = datetime.datetime.now()
+        pt1 = LocationPoint(t1, 1, 0)
+        pt2 = LocationPoint(t1, 0, 1)
+
+        res = pt1 + pt2
+        self.assertTrue(res._lat == 1 and res._lng == 1)
+
 
 if __name__ == "__main__":
     unittest.main()
