f74e1a04e77ef341782a94db59fa8429fffdeb8b
William DROZ <william.droz@idiap.ch>
2019-01-31 09:10:55 +0100

    test sub operator

--- test/test_LocationPoint.py
+++ test/test_LocationPoint.py
@@ -33,6 +33,14 @@ class LocationPointTestCase(unittest.TestCase):
         res = pt1 + pt2
         self.assertTrue(res._lat == 1 and res._lng == 1)
 
+    def test_sub(self):
+        t1 = datetime.datetime.now()
+        pt1 = LocationPoint(t1, 1, 0)
+        pt2 = LocationPoint(t1, 0, 1)
+
+        res = pt1 - pt2
+        self.assertTrue(res._lat == 1 and res._lng == -1)
+
 
 if __name__ == "__main__":
     unittest.main()
