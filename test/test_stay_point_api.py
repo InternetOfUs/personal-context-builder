9b20485dc38b5dc3c2249c9901a5a588ad7a2dfb
William DROZ <william.droz@idiap.ch>
2019-02-01 08:52:09 +0100

    mock testing stay point through sanic app

---
+++ test/test_stay_point_api.py
@@ -0,0 +1,18 @@
+import unittest
+
+from sanic_app import app
+import json
+
+
+class StayPointAPITestCase(unittest.TestCase):
+    def test_status_code_200(self):
+        _, response = app.test_client.post("/staypoints/")
+        self.assertEqual(response.status, 200)
+
+    def test_return_stay_points(self):
+        request, response = app.test_client.post("/staypoints/")
+        self.assertEqual(response.json, {"stay": "points"})
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
