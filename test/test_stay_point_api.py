21d34fd19501de4fc6f3afca5b9a8a3a04e27fd0
William DROZ <william.droz@idiap.ch>
2019-02-01 14:16:46 +0100

    test forwarded parameters

--- test/test_stay_point_api.py
+++ test/test_stay_point_api.py
@@ -2,6 +2,7 @@ import unittest
 import json
 import datetime
 from sanic_app import app
+import config
 
 
 class StayPointAPITestCase(unittest.TestCase):
@@ -48,6 +49,87 @@ class StayPointAPITestCase(unittest.TestCase):
         }
         _, response = app.test_client.post("/staypoints/", data=json.dumps(data))
         self.assertEqual(len(response.json.get("staypoints")), 1)
+
+    def test_custom_min_time_ms(self):
+        datetime_format = "%A, %d. %B %Y %I:%M%p"
+        t1 = datetime.datetime.now().strftime(datetime_format)
+        t2 = datetime.datetime.now().strftime(datetime_format)
+        data = {
+            "locations": [
+                {"pts_t": t1, "lat": 1, "lng": 2},
+                {"pts_t": t2, "lat": 1, "lng": 2},
+            ],
+            "datetime_format": datetime_format,
+            "time_min_ms": 10,
+        }
+        _, response = app.test_client.post("/staypoints/", data=json.dumps(data))
+        self.assertEqual(len(response.json.get("staypoints")), 0)
+
+    def test_custom_max_time_ms_outside_limit(self):
+        t1 = datetime.datetime.now().strftime(config.DEFAULT_DATETIME_FORMAT)
+        t2 = (datetime.datetime.now() + datetime.timedelta(seconds=5)).strftime(
+            config.DEFAULT_DATETIME_FORMAT
+        )
+        data = {
+            "locations": [
+                {"pts_t": t1, "lat": 1, "lng": 2},
+                {"pts_t": t2, "lat": 1, "lng": 2},
+            ],
+            "time_min_ms": -1,
+            "time_max_ms": 3000,
+        }
+        _, response = app.test_client.post("/staypoints/", data=json.dumps(data))
+        self.assertEqual(len(response.json.get("staypoints")), 0)
+
+    def test_custom_max_time_ms_inside_limit(self):
+        t1 = datetime.datetime.now().strftime(config.DEFAULT_DATETIME_FORMAT)
+        t2 = (datetime.datetime.now() + datetime.timedelta(seconds=5)).strftime(
+            config.DEFAULT_DATETIME_FORMAT
+        )
+        data = {
+            "locations": [
+                {"pts_t": t1, "lat": 1, "lng": 2},
+                {"pts_t": t2, "lat": 1, "lng": 2},
+            ],
+            "time_min_ms": -1,
+            "time_max_ms": 15000,
+        }
+        _, response = app.test_client.post("/staypoints/", data=json.dumps(data))
+        self.assertEqual(len(response.json.get("staypoints")), 1)
+
+    def test_custom_distance_max_m_inside_limit(self):
+        t1 = datetime.datetime.now().strftime(config.DEFAULT_DATETIME_FORMAT)
+        t2 = (datetime.datetime.now() + datetime.timedelta(seconds=5)).strftime(
+            config.DEFAULT_DATETIME_FORMAT
+        )
+        data = {
+            "locations": [
+                {"pts_t": t1, "lat": 1, "lng": 2},
+                {"pts_t": t2, "lat": 1, "lng": 2},
+            ],
+            "time_min_ms": -1,
+            "time_max_ms": 15000,
+            "distance_max_m": 10 ** 10,
+        }
+        _, response = app.test_client.post("/staypoints/", data=json.dumps(data))
+        self.assertEqual(len(response.json.get("staypoints")), 1)
+
+    def test_custom_distance_max_m_outside_limit(self):
+        t1 = datetime.datetime.now().strftime(config.DEFAULT_DATETIME_FORMAT)
+        t2 = (datetime.datetime.now() + datetime.timedelta(seconds=5)).strftime(
+            config.DEFAULT_DATETIME_FORMAT
+        )
+        data = {
+            "locations": [
+                {"pts_t": t1, "lat": 1, "lng": 2},
+                {"pts_t": t2, "lat": 10, "lng": 20},
+            ],
+            "time_min_ms": -1,
+            "time_max_ms": 15000,
+            "distance_max_m": 500,
+        }
+        _, response = app.test_client.post("/staypoints/", data=json.dumps(data))
+        self.assertEqual(len(response.json.get("staypoints")), 0)
 
 
 if __name__ == "__main__":  # pragma: no cover
