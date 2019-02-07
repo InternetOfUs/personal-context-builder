1f8bafbc5e4d298b62764bd3435f1075de903b46
William DROZ <william.droz@idiap.ch>
2019-02-07 10:20:29 +0100

    fix #2, stayregion api is working

--- sanic_app.py
+++ sanic_app.py
@@ -2,8 +2,8 @@ from sanic import Sanic
 from sanic.views import HTTPMethodView
 from sanic.response import text
 from sanic.response import json
-from wenet_models import LocationPoint
-from wenet_algo import estimate_stay_points
+from wenet_models import LocationPoint, StayPoint
+from wenet_algo import estimate_stay_points, estimate_stay_regions_a_day
 import config
 import datetime
 
@@ -41,6 +41,16 @@ def retreive_staypoints_parameters(req_json):
     return datetime_format, time_max_ms, time_min_ms, distance_max_m
 
 
+def retreive_stayregions_parameters(req_json):
+    datetime_format = get_field_if_exist(
+        req_json, "datetime_format", config.DEFAULT_DATETIME_FORMAT
+    )
+    distance_threshold_m = get_field_if_exist(
+        req_json, "distance_threshold_m", config.DEFAULT_STAYREGION_DISTANCE_THRESHOLD_M
+    )
+    return datetime_format, distance_threshold_m
+
+
 class StayPointsView(HTTPMethodView):
     def post(self, request):
         req_json = request.json
@@ -71,17 +81,35 @@ class StayPointsView(HTTPMethodView):
 
 
 class StayRegionsView(HTTPMethodView):
-    """
-    TODO test
-    """
-
     def post(self, request):
         req_json = request.json
         if not is_stayregions_request_valide(req_json):
             return json({})
+        datetime_format, distance_threshold_m = retreive_stayregions_parameters(
+            req_json
+        )
+        stay_points = []
+        for stay_point_dict in req_json["staypoints"]:
+            stay_point = StayPoint(
+                datetime.datetime.strptime(
+                    stay_point_dict["_t_start"], datetime_format
+                ),
+                datetime.datetime.strptime(stay_point_dict["_t_stop"], datetime_format),
+                stay_point_dict["_lat"],
+                stay_point_dict["_lng"],
+            )
+            stay_points.append(stay_point)
+
+        res = estimate_stay_regions_a_day(stay_points, distance_threshold_m)
+        res = [s.__dict__ for s in res]
+        for p in res:
+            p["_t_start"] = p["_t_start"].strftime(datetime_format)
+            p["_t_stop"] = p["_t_stop"].strftime(datetime_format)
+        return json({"stayregions": res})
 
 
 app.add_route(StayPointsView.as_view(), "/staypoints/")
+app.add_route(StayRegionsView.as_view(), "/stayregions/")
 
 
 if __name__ == "__main__":  # pragma: no cover
