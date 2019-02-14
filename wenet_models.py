877603441233aa038e768bd50366bb81d3d8dba9
William DROZ <william.droz@idiap.ch>
2019-02-14 11:10:17 +0100

    refactor to use a base class for all point-related model

--- wenet_models.py
+++ wenet_models.py
@@ -7,25 +7,14 @@ from wenet_tools import space_distance_m, time_difference_ms
 import numpy as np
 
 
-class LocationPoint(object):
+class GPSPoint(object):
     """
-    class that handle timestamped gps points
+    base class or all points-related models
     """
 
-    def __init__(self, pts_t, lat, lng):
-        self._pts_t = pts_t
+    def __init__(self, lat, lng):
         self._lat = lat
         self._lng = lng
-
-    def time_difference_ms(self, other):
-        """ Compute the time difference in (ms)
-
-        Args:
-            other: other LocationPoint instance to compare
-
-        Return: time difference in ms
-        """
-        return time_difference_ms(self._pts_t, other._pts_t)
 
     def space_distance_m(self, other):
         """ Compute the spatial distance in meters
@@ -42,60 +31,92 @@ class LocationPoint(object):
     def __add__(self, other):
         lat = self._lat + other._lat
         lng = self._lng + other._lng
-        return LocationPoint(self._pts_t, lat, lng)
+        return GPSPoint(lat, lng)
 
     def __sub__(self, other):
         lat = self._lat - other._lat
         lng = self._lng - other._lng
-        return LocationPoint(self._pts_t, lat, lng)
+        return GPSPoint(lat, lng)
 
     def __truediv__(self, other):
-        if isinstance(other, LocationPoint):
+        if isinstance(other, GPSPoint):
             lat = self._lat / other._lat
             lng = self._lng / other._lng
         else:
             lat = self._lat / other
             lng = self._lng / other
-        return LocationPoint(self._pts_t, lat, lng)
+        return GPSPoint(lat, lng)
 
     def __mul__(self, other):
-        if isinstance(other, LocationPoint):
+        if isinstance(other, GPSPoint):
             lat = self._lat * other._lat
             lng = self._lng * other._lng
         else:
             lat = self._lat * other
             lng = self._lng * other
-        return LocationPoint(self._pts_t, lat, lng)
+        return GPSPoint(lat, lng)
 
     def __hash__(self):
         return str(self).__hash__()
 
     def __str__(self):
-        return f"{self._lat:.5f}, {self._lng:.5f} [{self._pts_t}]"
+        return f"{self._lat:.5f}, {self._lng:.5f}"
 
     def __eq__(self, other):
         return self.__hash__() == other.__hash__()
 
 
-class StayPoint(object):
+class LocationPoint(GPSPoint):
+    """
+    class that handle timestamped gps points
+    """
+
+    def __init__(self, pts_t, lat, lng):
+        super().__init__(lat, lng)
+        self._pts_t = pts_t
+
+    def time_difference_ms(self, other):
+        """ Compute the time difference in (ms)
+
+        Args:
+            other: other LocationPoint instance to compare
+
+        Return: time difference in ms
+        """
+        return time_difference_ms(self._pts_t, other._pts_t)
+
+    def __add__(self, other):
+        point = super().__add__(other)
+        return LocationPoint(self._pts_t, point._lat, point._lng)
+
+    def __sub__(self, other):
+        point = super().__sub__(other)
+        return LocationPoint(self._pts_t, point._lat, point._lng)
+
+    def __truediv__(self, other):
+        point = super().__truediv__(other)
+        return LocationPoint(self._pts_t, point._lat, point._lng)
+
+    def __mul__(self, other):
+        point = super().__mul__(other)
+        return LocationPoint(self._pts_t, point._lat, point._lng)
+
+    def __str__(self):
+        return super().__str__() + f" [{self._pts_t}]"
+
+
+class StayPoint(GPSPoint):
     """
     class for stay point
     """
 
     def __init__(self, t_start, t_stop, lat, lng):
+        super().__init__(lat, lng)
         self._t_start = t_start
         self._t_stop = t_stop
-        self._lat = lat
-        self._lng = lng
-
-    def __hash__(self):
-        return str(self).__hash__()
 
     def __str__(self):
-        return f"{self._lat:.5f}, {self._lng:.5f} [{self._t_start} to {self._t_stop}]"
-
-    def __eq__(self, other):
-        return self.__hash__() == other.__hash__()
+        return super().__str__() + f"[{self._t_start} to {self._t_stop}]"
 
 
 class StayRegion(StayPoint):
