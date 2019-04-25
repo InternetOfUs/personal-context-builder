f41d4b9c8011629ee5587f5e4dc8fb01a3ef99e0
William DROZ <william.droz@idiap.ch>
2019-04-25 09:00:42 +0200

    can override config from env

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 Config for the project
 """
 
+from os import environ
+
 DEFAULT_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
 
 DEFAULT_STAYPOINTS_TIME_MIN_MS = 5 * 60 * 1000
@@ -20,3 +22,22 @@ DEFAULT_APP_PORT = 8000
 
 DEFAULT_REDIS_HOST = "localhost"
 DEFAULT_REDIS_PORT = 6379
+
+
+def _update_parameters_from_env():
+    """ update the config values from env
+    """
+    for k, v in globals().items():
+        if k.startswith("DEFAULT_"):
+            if k in environ:
+                new_v = type(v)(environ[k])
+                print(
+                    "Parameters {} was updated from {} to {} by environment override".format(
+                        k, v, new_v
+                    )
+                )
+                globals()[k] = new_v
+
+
+# update the config by using env
+_update_parameters_from_env()
