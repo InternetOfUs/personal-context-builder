2ecd5455964bbe99d1d4aef27a5b222c572f57bd
William DROZ <william.droz@idiap.ch>
2019-04-30 07:29:24 +0200

    add test for BaseModelWrapper

---
+++ test/test_analysis_models.py
@@ -0,0 +1,35 @@
+import unittest
+from os import remove
+from sklearn.datasets import load_iris
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from wenet_analysis_models import BaseModelWrapper
+from functools import partial
+
+
+class UserProfileDBTestCase(unittest.TestCase):
+    def setUp(self):
+        self.model_1 = "model_1.bin"
+
+    def test_simple_model(self):
+        iris = load_iris()
+        X = iris.data
+        y = iris.target
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.33, random_state=42
+        )
+        my_svn = partial(SVC, random_state=42)
+        my_wrapper = BaseModelWrapper(my_svn, "simple SVN")
+        my_wrapper.fit(X_train, y_train)
+        prediction_1 = my_wrapper.predict(X_test)
+        my_wrapper.save(self.model_1)
+        my_wrapper2 = BaseModelWrapper.load(self.model_1)
+        prediction_2 = my_wrapper2.predict(X_test)
+        self.assertTrue((prediction_1 == prediction_2).all())
+
+    def tearDown(self):
+        remove(self.model_1)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
