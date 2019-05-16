e7bb7c4287864c73554e9fc525e6dd9ac4803ba8
William DROZ <william.droz@idiap.ch>
2019-05-16 09:52:45 +0200

    add new functionalities (show profiles and mock options

--- wenet_cli_entrypoint.py
+++ wenet_cli_entrypoint.py
@@ -11,29 +11,48 @@ from wenet_trainer import BaseBOWTrainer, BaseModelTrainer
 from wenet_analysis_models import SimpleLDA
 from wenet_data_loading import MockWenetSourceLabels, MockWenetSourceLocations
 from wenet_profiles_writer import ProfileWritterFromMock
+from wenet_user_profile_db import get_all_profiles, get_profile
 
 
-def train():
-    source_locations = MockWenetSourceLocations()
-    source_labels = MockWenetSourceLabels(source_locations)
-    bow_trainer = BaseBOWTrainer(source_locations, source_labels)
-    lda_model_untrained = SimpleLDA()
-    model_trainer = BaseModelTrainer(
-        source_locations, source_labels, bow_trainer, lda_model_untrained
-    )
-    lda_model = model_trainer.train()
-    lda_model.save(filename="last_lda.p")
+def train(is_mock=False):
+    if is_mock:
+        print(f"Training on mock data")
+        source_locations = MockWenetSourceLocations()
+        source_labels = MockWenetSourceLabels(source_locations)
+        bow_trainer = BaseBOWTrainer(source_locations, source_labels)
+        lda_model_untrained = SimpleLDA()
+        model_trainer = BaseModelTrainer(
+            source_locations, source_labels, bow_trainer, lda_model_untrained
+        )
+        lda_model = model_trainer.train()
+        lda_model.save(filename="last_lda.p")
+        print(f"done")
 
 
-def update():
-    source_locations = MockWenetSourceLocations()
-    source_labels = MockWenetSourceLabels(source_locations)
-    bow_trainer = BaseBOWTrainer(source_locations, source_labels)
-    model = SimpleLDA.load("last_lda.p")
-    profile_writter = ProfileWritterFromMock(
-        source_locations, source_labels, model, bow_trainer
-    )
-    profile_writter.update_profiles()
+def update(is_mock=False):
+    if is_mock:
+        print(f"updating profiles on mock data")
+        source_locations = MockWenetSourceLocations()
+        source_labels = MockWenetSourceLabels(source_locations)
+        bow_trainer = BaseBOWTrainer(source_locations, source_labels)
+        model = SimpleLDA.load("last_lda.p")
+        profile_writter = ProfileWritterFromMock(
+            source_locations, source_labels, model, bow_trainer
+        )
+        profile_writter.update_profiles()
+        print(f"done")
+
+
+def show_profile(user_id):
+    profile = get_profile(user_id)
+    print(f"[{user_id}] {profile}")
+
+
+def show_all_profiles():
+    users_profiles = get_all_profiles()
+    print(f"number of profiles : {len(users_profiles)}")
+    for user_id, profile in users_profiles.items():
+        print(f"[{user_id}] {profile}")
 
 
 if __name__ == "__main__":
@@ -57,8 +76,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     if args.train:
-        train()
+        train(args.mock)
     if args.update:
-        update()
+        update(args.mock)
+    if args.show_all:
+        show_all_profiles()
     if args.app_run:
         pass  # TODO run the services
