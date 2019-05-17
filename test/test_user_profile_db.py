07831de741bee36b3ba299f3c74ad9d55c2c08e1
William DROZ <william.droz@idiap.ch>
2019-05-17 10:00:21 +0200

    update the tests to use only the class version of the db access

--- test/test_user_profile_db.py
+++ test/test_user_profile_db.py
@@ -1,11 +1,5 @@
 import unittest
-from wenet_user_profile_db import (
-    set_profile,
-    get_profile,
-    delete_profile,
-    set_profiles,
-    get_all_profiles,
-)
+from wenet_user_profile_db import DatabaseProfileHandler
 
 
 class UserProfileDBTestCase(unittest.TestCase):
@@ -19,38 +13,42 @@ class UserProfileDBTestCase(unittest.TestCase):
 
     def test_create_retreive_user(self):
         user_vector = [0, 1, 1, 0, 1, 0.5]
-        set_profile(self.user_1, user_vector)
-        res = get_profile(self.user_1)
+        DatabaseProfileHandler.get_instance().set_profile(self.user_1, user_vector)
+        res = DatabaseProfileHandler.get_instance().get_profile(self.user_1)
         self.assertEqual(user_vector, res)
 
     def test_delete_user(self):
         user_vector = [1, 0.5, 1, 0, 0, 0]
-        set_profile(self.user_2, user_vector)
-        delete_profile(self.user_2)
-        res = get_profile(self.user_2)
+        DatabaseProfileHandler.get_instance().set_profile(self.user_2, user_vector)
+        DatabaseProfileHandler.get_instance().delete_profile(self.user_2)
+        res = DatabaseProfileHandler.get_instance().get_profile(self.user_2)
         self.assertIsNone(res)
 
     def test_set_profiles(self):
         v1 = [0, 1, 0, 1]
         v2 = [1, 0, 1, 0]
-        set_profiles([self.user_3, self.user_4], [v1, v2])
-        res = get_profile(self.user_4)
+        DatabaseProfileHandler.get_instance().set_profiles(
+            [self.user_3, self.user_4], [v1, v2]
+        )
+        res = DatabaseProfileHandler.get_instance().get_profile(self.user_4)
         self.assertEqual(v2, res)
 
     def test_get_all_profiles(self):
         v1 = [0, 1, 0, 1]
         v2 = [1, 0, 1, 0]
-        set_profiles([self.user_5, self.user_6], [v1, v2])
-        res = get_all_profiles(match="test_*")
+        DatabaseProfileHandler.get_instance().set_profiles(
+            [self.user_5, self.user_6], [v1, v2]
+        )
+        res = DatabaseProfileHandler.get_instance().get_all_profiles(match="test_*")
         self.assertEqual(v2, res[self.user_6])
 
     def tearDown(self):
-        delete_profile(self.user_1)
+        DatabaseProfileHandler.get_instance().delete_profile(self.user_1)
         #  2 is deleted in another testcase
-        delete_profile(self.user_3)
-        delete_profile(self.user_4)
-        delete_profile(self.user_5)
-        delete_profile(self.user_6)
+        DatabaseProfileHandler.get_instance().delete_profile(self.user_3)
+        DatabaseProfileHandler.get_instance().delete_profile(self.user_4)
+        DatabaseProfileHandler.get_instance().delete_profile(self.user_5)
+        DatabaseProfileHandler.get_instance().delete_profile(self.user_6)
 
 
 if __name__ == "__main__":  # pragma: no cover
