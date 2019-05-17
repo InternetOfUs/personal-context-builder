8d5ef0a543d31c0b2c2214ad7469819c4bb12df2
William DROZ <william.droz@idiap.ch>
2019-05-17 12:50:16 +0200

    add tests for save/load of BagOfWordsVectorizer

--- test/test_BagOfWordsVectorizer.py
+++ test/test_BagOfWordsVectorizer.py
@@ -1,9 +1,21 @@
 from wenet_analysis import BagOfWordsVectorizer
-from wenet_data_loading import MockWenetSourceLocations
+from wenet_data_loading import MockWenetSourceLocations, MockWenetSourceLabels
 import unittest
+from os import remove
 
 
 class BagOfWordsVectorizerTestCase(unittest.TestCase):
+    def test_save_load(self):
+        source_locations = MockWenetSourceLocations()
+        source_labels = MockWenetSourceLabels(source_locations)
+        vectorizer = BagOfWordsVectorizer(source_locations, source_labels)
+        regions_size = vectorizer._inner_vector_size
+        filename = "bow_delete_me.p"
+        vectorizer.save(filename)
+        vectorizer = BagOfWordsVectorizer.load(filename)
+        remove(filename)
+        self.assertEqual(regions_size, vectorizer._inner_vector_size)
+
     def test_group_by_days(self):
         locations = MockWenetSourceLocations._create_fake_locations("test_user", 10)
         days_locations = BagOfWordsVectorizer.group_by_days(
