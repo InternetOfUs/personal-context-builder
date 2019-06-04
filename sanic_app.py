f2698073d1d3035d774b366fe8cab4640f2d0b93
William DROZ <william.droz@idiap.ch>
2019-06-04 10:25:58 +0200

    change blueprint order and add API documentation

--- sanic_app.py
+++ sanic_app.py
@@ -13,12 +13,15 @@ class WenetApp(object):
         virtual_host_location=config.DEFAULT_VIRTUAL_HOST_LOCATION,
     ):
         self._app = Sanic(app_name)
-        routines_bp = create_routines_bp(virtual_host_location)
-        models_bp = create_available_models_bp(virtual_host_location)
 
         swagger_blueprint.url_prefix = virtual_host_location
 
+        routines_bp = create_routines_bp(virtual_host_location)
+        models_bp = create_available_models_bp(virtual_host_location)
+
         self._app.blueprint([swagger_blueprint, routines_bp, models_bp])
+        self._app.config.API_HOST = virtual_host
+        self._app.config.API_BASEPATH = virtual_host_location
 
     def run(self, host=config.DEFAULT_APP_INTERFACE, port=config.DEFAULT_APP_PORT):
         self._app.run(host, port)
