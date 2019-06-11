464396edc0f7075bb82be46333b666d7f045d5e5
William DROZ <william.droz@idiap.ch>
2019-06-11 11:09:47 +0200

    create logger config instead of just logger

--- wenet_logger.py
+++ wenet_logger.py
@@ -6,6 +6,7 @@ use syslog
 import logging
 import logging.handlers
 import config
+import sys
 
 
 def create_logger(name="wenet-undefined"):
@@ -27,19 +28,60 @@ def create_logger(name="wenet-undefined"):
     return logger
 
 
-def create_web_logger(name="wenet-App"):
+def create_web_logger_config():
     """ create a logger for the sanic app
     """
-    logger = logging.getLogger(name)
-    logger.setLevel(config.DEFAULT_LOGGER_LEVEL)
-    formatter = logging.Formatter(config.DEFAULT_SANIC_LOGGER_FORMAT)
-
-    syslog = logging.handlers.SysLogHandler("/dev/log")
-    ch = logging.StreamHandler()
-
-    syslog.formatter = formatter
-    ch.formatter = formatter
-
-    logger.addHandler(syslog)
-    logger.addHandler(ch)
-    return logger
+    return dict(
+        version=1,
+        disable_existing_loggers=False,
+        loggers={
+            "sanic.root": {"level": "INFO", "handlers": ["console"]},
+            "sanic.error": {
+                "level": "INFO",
+                "handlers": ["error_console"],
+                "propagate": True,
+                "qualname": "sanic.error",
+            },
+            "sanic.access": {
+                "level": "INFO",
+                "handlers": ["access_console"],
+                "propagate": True,
+                "qualname": "sanic.access",
+            },
+        },
+        handlers={
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "generic",
+                "stream": sys.stdout,
+            },
+            "error_console": {
+                "class": "logging.StreamHandler",
+                "formatter": "generic",
+                "stream": sys.stderr,
+            },
+            "access_console": {
+                "class": "logging.StreamHandler",
+                "formatter": "access",
+                "stream": sys.stdout,
+            },
+            "sys-logger0": {
+                "class": "logging.handlers.SysLogHandler",
+                "address": "/dev/log",
+                "facility": "user",
+                "formatter": "access",
+            },
+        },
+        formatters={
+            "generic": {
+                "format": config.DEFAULT_LOGGER_FORMAT,
+                "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+                "class": "logging.Formatter",
+            },
+            "access": {
+                "format": config.DEFAULT_SANIC_LOGGER_FORMAT,
+                "datefmt": "[%Y-%m-%d %H:%M:%S %z]",
+                "class": "logging.Formatter",
+            },
+        },
+    )
