b380e18434135a67ee4a94ede9d7b0b0efea8554
William DROZ <william.droz@idiap.ch>
2019-06-13 14:56:48 +0200

    add docstring for WenetApp

--- sanic_app.py
+++ sanic_app.py
@@ -13,12 +13,21 @@ _LOGGER_CONFIG = create_web_logger_config()
 
 
 class WenetApp(object):
+    """ class that create the Sanic web service
+    """
+
     def __init__(
         self,
         app_name=config.DEFAULT_APP_NAME,
         virtual_host=config.DEFAULT_VIRTUAL_HOST,
         virtual_host_location=config.DEFAULT_VIRTUAL_HOST_LOCATION,
     ):
+        """ constructor
+        Args:
+        app_name -- name of the app
+        virtual_host -- virtual host (can be provided by nginx) e. g. lab.idiap.ch
+        virtual_host_location -- virtual host location (can be provided by nginx) e. g. /devel/wenet/
+        """
         self._app = Sanic(app_name)
 
         routines_bp = create_routines_bp(virtual_host_location)
@@ -27,6 +36,11 @@ class WenetApp(object):
         self._app.blueprint([routines_bp, models_bp])
 
     def run(self, host=config.DEFAULT_APP_INTERFACE, port=config.DEFAULT_APP_PORT):
+        """ run the web service
+        Args:
+        host -- the host/interface to listen
+        port -- which port to use
+        """
         logging.config.dictConfig(_LOGGER_CONFIG)
 
         self._app.error_handler.add(NotFound, wenet_exceptions.ignore_404s)
