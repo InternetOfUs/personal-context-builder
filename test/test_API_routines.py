00a206f16d9fdad2af81cc230eab43d0657d263e
William DROZ <william.droz@idiap.ch>
2019-06-13 16:21:18 +0200

    test routines

---
+++ test/test_API_routines.py
@@ -0,0 +1,35 @@
+import unittest
+from sanic_app import WenetApp
+from wenet_cli_entrypoint import update, train
+import config
+
+
+class APIRoutinesTestCase(unittest.TestCase):
+    def setUp(self):
+        self._app = WenetApp("test wenet", is_mock=True)._app
+        train(is_mock=True)
+        update(is_mock=True)
+
+    def test_simple_lda_exist(self):
+        _, response = self._app.test_client.get(
+            config.DEFAULT_VIRTUAL_HOST_LOCATION + "/routines/"
+        )
+        self.assertIn("SimpleLDA", response.json)
+
+    def test_mock_user_1(self):
+        _, response = self._app.test_client.get(
+            config.DEFAULT_VIRTUAL_HOST_LOCATION + "/routines/"
+        )
+        self.assertIn("mock_user_1", response.json.get("SimpleLDA"))
+
+    def test_not_mock_user_1(self):
+        _, response = self._app.test_client.get(
+            config.DEFAULT_VIRTUAL_HOST_LOCATION + "/routines/mock_user_1/"
+        )
+        self.assertNotIn("mock_user_2", response.json.get("SimpleLDA"))
+
+    def test_mock_user_1_with_mock_user_1(self):
+        _, response = self._app.test_client.get(
+            config.DEFAULT_VIRTUAL_HOST_LOCATION + "/routines/mock_user_1/"
+        )
+        self.assertIn("mock_user_1", response.json.get("SimpleLDA"))
