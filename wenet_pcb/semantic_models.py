0ecece2d4f65855bb2a5912af8d9f9be623614ea
William DROZ <william.droz@idiap.ch>
2019-10-16 16:10:57 +0200

    Template for semantic models

---
+++ wenet_pcb/semantic_models.py
@@ -0,0 +1,6 @@
+""" module that contains sematic model for the predictions
+
+-  Train on history of the user days with locations and labels
+-  Should be able to predict labels probabilities for each time slot for each weekday
+-  Should be able to give scores/metrics for validation
+"""
