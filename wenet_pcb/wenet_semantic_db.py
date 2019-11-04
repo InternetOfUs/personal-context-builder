c735726dddb8da3f318e558404530a2bd4271f9a
William DROZ <william.droz@idiap.ch>
2019-11-04 09:24:24 +0100

    add to_dict methods to convert embedded results into dict

--- wenet_pcb/wenet_semantic_db.py
+++ wenet_pcb/wenet_semantic_db.py
@@ -9,6 +9,7 @@ from wenet_pcb.wenet_logger import create_logger
 _LOGGER = create_logger(__name__)
 _Base = declarative_base()
 
+
 class LabelsLocation(_Base):
     __tablename__ = "labels_locations"
     id = Column(Integer, primary_key=True)
@@ -16,12 +17,26 @@ class LabelsLocation(_Base):
     lng = Column(Float)
     label_id = Column(Integer, ForeignKey("labels.id"))
 
+    def to_dict(self):
+        my_dict = dict()
+        my_dict["id"] = self.id
+        my_dict["lat"] = self.lat
+        my_dict["lng"] = self.lng
+        my_dict["label_id"] = self.label_id
+        return my_dict
+
 
 class Labels(_Base):
     __tablename__ = "labels"
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+
+    def to_dict(self):
+        my_dict = dict()
+        my_dict["id"] = self.id
+        my_dict["name"] = self.name
+        return my_dict
 
 
 class LabelsScore(_Base):
@@ -32,6 +47,14 @@ class LabelsScore(_Base):
     label_location_id = Column(Integer, ForeignKey("labels_locations.id"))
     score = Column(Float)
 
+    def to_dict(self):
+        my_dict = dict()
+        my_dict["id"] = self.id
+        my_dict["semantic_routine_id"] = self.semantic_routine_id
+        my_dict["label_location_id"] = self.label_location_id
+        my_dict["score"] = self.score
+        return my_dict
+
 
 class SemanticRoutine(_Base):
     __tablename__ = "semantic_routines"
@@ -41,6 +64,15 @@ class SemanticRoutine(_Base):
     weekday = Column(Integer)
     time_slot = Column(String)
     label_scores = relationship(LabelsScore)
+
+    def to_dict(self):
+        my_dict = dict()
+        my_dict["id"] = self.id
+        my_dict["user_id"] = self.user_id
+        my_dict["weekday"] = self.weekday
+        my_dict["time_slot"] = self.time_slot
+        my_dict["labels_scores"] = [row.to_dict() for row in self.label_scores]
+        return my_dict
 
 
 class SemanticRoutineDB(object):
@@ -75,15 +107,19 @@ class SemanticRoutineDB(object):
         for id, name in labels_dict.items():
             self.set_label(id, name)
 
+    def add_label_location(self, lat, lng, label_id):
+        location_label = LabelsLocation(lat=lat, lng=lng, label_id=label_id)
+        self._session.add(location_label)
+
     def get_labels(self):
-        return self._session.query(Labels).all()
+        return [row.to_dict() for row in self._session.query(Labels).all()]
 
     def add_semantic_routine(self, user_id, weekday, time_slot, labels_scores_dict):
         semantic_routine = SemanticRoutine(
             user_id=user_id, weekday=weekday, time_slot=time_slot
         )
-        for label_id, score in labels_scores_dict.items():
-            label_score = LabelsScore(label_id=label_id, score=score)
+        for label_location_id, score in labels_scores_dict.items():
+            label_score = LabelsScore(label_location_id=label_location_id, score=score)
             semantic_routine.label_scores.append(label_score)
             self._session.add(label_score)
         self._session.add(semantic_routine)
@@ -103,8 +139,7 @@ class SemanticRoutineDB(object):
                 .options(joinedload("label_scores"))
                 .all()
             )
-        # TODO get subitem
-        return res
+        return [row.to_dict() for row in res]
 
     @classmethod
     def get_instance(cls, is_mock=False):
@@ -117,8 +152,10 @@ if __name__ == "__main__":
     semantic_routine_db = SemanticRoutineDB.get_instance(is_mock=True)
     semantic_routine_db.set_labels({1: "HOME", 2: "WORK"})
     for row in semantic_routine_db.get_labels():
-        print(f"{row.id} -> {row.name}")
+        print(f"{row['id']} -> {row['name']}")
 
+    semantic_routine_db.add_label_location(30, 30, 1)
+    semantic_routine_db.add_label_location(40, 40, 2)
     semantic_routine_db.add_semantic_routine("toto", 1, "11:00", {1: 0.3, 2: 0.7})
     for row in semantic_routine_db.get_semantic_routines():
-        print(f"res : {row.__dict__}")
+        print(f"res : {row}")
