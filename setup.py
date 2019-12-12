b27b717d62d31cd8d17776edb62ff79a461dc09b
William DROZ <william.droz@idiap.ch>
2019-12-12 15:15:04 +0100

    set version 0.0.6

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 with open("requirements.txt", "r") as f:
-    requirements = (f.read().split("\n"))
+    requirements = f.read().split("\n")
 
 setuptools.setup(
     name="wenet_pcb",
-    version="0.0.4",
+    version="0.0.6",
     author="Idiap - William Droz",
     author_email="william.droz@idiap.ch",
     description="Personal Context Builder for the Wenet project",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    ],
-    install_requires=requirements
+    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=requirements,
 )
