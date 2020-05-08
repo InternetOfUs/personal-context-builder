9e90a52ed0687386c7a6721adaa42f964a81ff10
William Droz <william.droz@idiap.ch>
2020-05-08 09:04:50 +0200

    print empty routines

--- personal_context_builder/wenet_profile_manager.py
+++ personal_context_builder/wenet_profile_manager.py
@@ -2,6 +2,8 @@
     the routines
 """
 from personal_context_builder import config
+import requests
+from pprint import pprint
 
 
 class PersonalBehavior(object):
@@ -13,5 +15,12 @@ class PersonalBehavior(object):
         self.to_time = 2359
 
 
-def update_profile(routines, url=config.DEFAULT_PROFILE_MANAGER_URL):
-    pass
+def update_profile(routines, profile_id, url=config.DEFAULT_PROFILE_MANAGER_URL):
+    profile_url = url + f"/profiles/{profile_id}"
+    personal_behaviors = [PersonalBehavior(routine).__dict__ for routine in routines]
+    #  r = requests.put(profile_url, data={"personalBehaviors": personal_behaviors})
+    print(f"send to {profile_url}: \n")
+    for p in personal_behaviors:
+        pprint(p)
+
+    print()
