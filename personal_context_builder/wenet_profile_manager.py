8b2af219642edcbb7ae9c22f5ab501c66685f4b6
William Droz <william.droz@idiap.ch>
2020-05-20 14:32:26 +0200

    create classes that interface with Profile Manager

--- personal_context_builder/wenet_profile_manager.py
+++ personal_context_builder/wenet_profile_manager.py
@@ -3,21 +3,39 @@
 """
 from personal_context_builder import config
 import requests
+from collections import defaultdict
 from pprint import pprint
 
 
+class Label(object):
+    def __init__(self, name, semantic_class, latitude, longitude):
+        self.name = name
+        self.semantic_class = semantic_class
+        self.latitude = latitude
+        self.longitude = longitude
+
+
+class ScoredLabel(object):
+    def __init__(self, score, label):
+        self.label = label
+        self.score = score
+
+
 class PersonalBehavior(object):
-    def __init__(self, routine):
-        self.id = ""
-        self.label = ""
-        self.proximity = ""
-        self.from_time = 0
-        self.to_time = 2359
+    def __init__(self, user_id, weekday, confidence):
+        self.user_id = user_id
+        self.weekday = weekday
+        self.confidence = confidence
+        self.label_distribution = defaultdict(list)
+
+    def fill(self, routines):
+        #  TODO something with routines
+        pass
 
 
 def update_profile(routines, profile_id, url=config.DEFAULT_PROFILE_MANAGER_URL):
     profile_url = url + f"/profiles/{profile_id}"
-    personal_behaviors = [PersonalBehavior(routine).__dict__ for routine in routines]
+    personal_behaviors = []
     #  r = requests.put(profile_url, data={"personalBehaviors": personal_behaviors})
     print(f"send to {profile_url}: \n")
     for p in personal_behaviors:
