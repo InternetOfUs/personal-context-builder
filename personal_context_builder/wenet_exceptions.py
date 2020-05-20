f5d6775a6b10e072f0c2821c4b1a40fbe8e98d60
William Droz <william.droz@idiap.ch>
2020-05-20 14:03:58 +0200

    add wenet exceptions

--- personal_context_builder/wenet_exceptions.py
+++ personal_context_builder/wenet_exceptions.py
@@ -7,6 +7,22 @@ from personal_context_builder import config
 _LOGGER = create_logger(__name__)
 
 
+class WenetError(Exception):
+    """ Generic winet exception
+    """
+
+    def __init__(self, message="Generic Wenet Error"):
+        super().__init__(message)
+
+
+class WenetUpdateProfileManagerError(WenetError):
+    """ Can't update the profile manager
+    """
+
+    def __init__(self, message="Fail to update profile manager"):
+        super().__init__(message)
+
+
 async def ignore_404s(request, exception):
     error_code = 404
     return text(f"{error_code} - no route to {format(request.url)}", status=error_code)
