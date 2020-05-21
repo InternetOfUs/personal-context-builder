70fd449f8fd16e3302b288db85b02d346051f6c5
William Droz <william.droz@idiap.ch>
2020-05-21 17:33:45 +0200

    (WIP) work on StreambaseLabelsLoader to loads surveys from streambase

--- personal_context_builder/wenet_profile_manager.py
+++ personal_context_builder/wenet_profile_manager.py
@@ -16,7 +16,7 @@ from requests.exceptions import RequestException
 
 _LOGGER = create_logger(__name__)
 
-# TODO change me remove this once data is ok
+# TODO change me remove this once data are ok
 GPS_STR_FAKE = """{
   "userId": "1",
   "experimentId": "Wenet",
@@ -37,6 +37,110 @@ GPS_STR_FAKE = """{
     }
   ]
 }"""
+
+# TODO change me remove this once surveys are ok
+SURVEY_STR_FAKE = """{
+  "userId": "1",
+  "experimentId": "Wenet",
+  "properties": [
+    {
+      "timediariesanswers": [
+        {
+          "experimentId": "Wenet",
+          "userId": "1",
+          "instanceid": "f161dee2a122af926a9c4285275800942d128c34",
+          "answer": [
+            [
+              {
+                "cnt": "I am at home",
+                "qid": 2,
+                "aid": 3,
+                "cid": -1
+              }
+            ]
+          ],
+          "answerduration": 20142,
+          "answertimestamp": 20190909010129450,
+          "day": 20190910,
+          "instancetimestamp": 20190911201814344,
+          "notificationtimestamp": 20190911201816910,
+          "payload": [
+            {
+              "payload": {},
+              "qid": 1
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "timediariesquestions": [
+        {
+          "experimentId": "Wenet",
+          "userId": "1",
+          "instanceid": "f161dee2a122af926a9c4285275800942d128c34",
+          "question": {
+            "q": {
+              "id": 1,
+              "c": [],
+              "t": "t",
+              "at": "s",
+              "p": [
+                {
+                  "l": "en_US",
+                  "t": "What are you doing?"
+                },
+                {
+                  "l": "it_IT",
+                  "t": "Cosa stai facendo?"
+                }
+              ]
+            },
+            "a": [
+              [
+                {
+                  "id": 1,
+                  "c": [],
+                  "c_id": -1,
+                  "p": [
+                    {
+                      "l": "en_US",
+                      "t": "I am working"
+                    },
+                    {
+                      "l": "it_IT",
+                      "t": "Sto lavorando"
+                    }
+                  ]
+                },
+                {
+                  "id": 2,
+                  "c": [],
+                  "c_id": -1,
+                  "p": [
+                    {
+                      "l": "en_US",
+                      "t": "I am studying"
+                    },
+                    {
+                      "l": "it_IT",
+                      "t": "Sto studiando"
+                    }
+                  ]
+                }
+              ]
+            ]
+          },
+          "day": 20190910,
+          "instancetimestamp": 20190911201814344,
+          "status": "success",
+          "title": "Question Title"
+        }
+      ]
+    }
+  ]
+}
+"""
 
 
 class Label(object):
@@ -147,6 +251,100 @@ class StreamBaseLocationsLoader(BaseSourceLocations):
     def get_locations_all_users(self, max_n=None):
         return [
             l for locations in self._users_locations.values() for l in locations[:max_n]
+        ]
+
+
+class StreambaseLabelsLoader(BaseSourceLabels):
+    def __init__(self, location_loader, name="Streambase labels loader", last_days=14):
+        super().__init__(name)
+        self._location_loader = location_loader
+        self._url = config.DEFAULT_STREAMBASE_BATCH_URL
+        self._users_places = dict()
+        self._users_staypoints = dict()
+        self._users_stayregions = dict()
+        for user in self.get_users():
+            try:
+                user_url = self._url + user
+                surveys = self._load_survey(
+                    user=user, url=user_url, last_days=last_days
+                )
+                locations = location_loader.get_locations(user)
+                stay_points = estimate_stay_points(locations)
+                stay_points = sorted(stay_points, key=lambda sp: sp._t_start)
+                stay_regions = estimate_stay_regions(
+                    stay_points, distance_threshold_m=20
+                )
+                self._users_staypoints[user] = stay_points
+                self._users_stayregions[user] = stay_regions
+                self._users_places[user] = self._load_user_places(
+                    user, surveys, stay_regions
+                )
+            except:
+                pass
+
+    @staticmethod
+    def _load_survey(user, url, last_days):
+        parameters = dict()
+        date_to = datetime.datetime.now()
+        date_from = date_to - datetime.timedelta(hours=24 * last_days)
+        date_to_str = date_to.strftime("%Y%m%d")
+        date_from_str = date_from.strftime("%Y%m%d")
+        survey_streambase = json.loads(SURVEY_STR_FAKE)
+        # TODO change me to get token from partner?
+        parameters["Authorization"] = "Authorization"
+        parameters["from"] = date_from_str
+        parameters["to"] = date_to_str
+        parameters["properties"] = "tasksanswers"
+        try:
+            r = requests.get(url, data=parameters)
+            if r.code == 200:
+                _LOGGER.debug(
+                    f"request to stream base success for user {user} -  {r.json()}"
+                )
+                return r.json()
+            else:
+                _LOGGER.warn(
+                    f"request to stream base failed for user {user} with code {r.code}"
+                )
+        except RequestException:
+            _LOGGER.warn(f"request to stream base failed for user {user}")
+
+        # TODO change me when survey onlines
+        return survey_streambase
+
+    @staticmethod
+    def _load_user_places(user, surveys, staypoints):
+        ids_where_are_you = set([12])
+        answers_valid = set(["I am at home", "I am working", "I am studying"])
+        user_places = []
+        for index, row in df.iterrows():
+            question_id = row["question_id"]
+            if question_id not in ids_where_are_you:
+                continue
+            #  place = row['answer_id']
+            place = row["answer_content"]
+            if place not in answers_valid:
+                continue
+            pts_t = datetime.datetime.strptime(
+                str(row["answertimestamp"])[:-3], "%Y%m%d%H%M%S"
+            )
+            user_place_time_only = UserPlaceTimeOnly(pts_t, place, user)
+            user_place = user_place_time_only.to_user_place_from_stay_points(staypoints)
+            if user_place is not None:
+                user_places.append(user_place)
+        return user_places
+
+    def get_users(self):
+        return self._location_loader.get_users()
+
+    def get_labels(self, user_id, max_n=None):
+        return self._users_places[user_id][:max_n]
+
+    def get_labels_all_users(self, max_n=None):
+        return [
+            user_place
+            for sublist in self._users_places.values()
+            for user_place in sublist
         ]
 
 
