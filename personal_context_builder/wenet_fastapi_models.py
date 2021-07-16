a671ecc946695a8920b4f6117306f566c4298d88
William DROZ <william.droz@idiap.ch>
2021-07-16 15:06:25 +0200

    (WIP) add EmbeddedModelName and EmbeddedRoutineOut as pydantic models

---
+++ personal_context_builder/wenet_fastapi_models.py
@@ -0,0 +1,22 @@
+""" module for fastapi models
+
+Copyright (c) 2021 Idiap Research Institute, https://www.idiap.ch/
+Written by William Droz <william.droz@idiap.ch>,
+
+"""
+from pydantic import BaseModel
+from typing import Dict, List
+
+
+class EmbeddedModelName(str):
+    """name of the model"""
+
+
+class EmbeddedRoutineOut(BaseModel, Dict[str, Dict[str, List[float]]]):
+    class Config:
+        schema_extra = {
+            "example": {
+                "SimpleLDA": {"mock_user_1": [0, 1, 0, 0.5, 1]},
+                "SimpleBOW": {"mock_user_1": [1, 1, 0, 0.5, 1]},
+            }
+        }
