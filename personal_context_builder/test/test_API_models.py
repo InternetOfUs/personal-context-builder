ad8447a47f1bd716face03ccb0dab5c08c3ad061
William DROZ <william.droz@idiap.ch>
2021-07-19 11:17:36 +0200

    test for the models API (fastapi)

---
+++ personal_context_builder/test/test_API_models.py
@@ -0,0 +1,24 @@
+""" Test for the API that handle models
+
+Copyright (c) 2021 Idiap Research Institute, https://www.idiap.ch/
+Written by William Droz <william.droz@idiap.ch>,
+"""
+import unittest
+
+from personal_context_builder import config
+from personal_context_builder.wenet_fastapi_app import app
+
+from fastapi.testclient import TestClient
+
+
+class APIModelsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_simple_lda_exist(self):
+        response = self.client.get(config.PCB_VIRTUAL_HOST_LOCATION + "/models/")
+        self.assertIn("SimpleLDA", response.json())
+
+    def test_simple_bow_exist(self):
+        response = self.client.get(config.PCB_VIRTUAL_HOST_LOCATION + "/models/")
+        self.assertIn("SimpleBOW", response.json())
