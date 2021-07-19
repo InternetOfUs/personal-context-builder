c0f421e6dba8d14beadf6e355f808ad469901d06
William DROZ <william.droz@idiap.ch>
2021-07-19 14:00:18 +0200

    use named argument for db_index

--- personal_context_builder/wenet_analysis.py
+++ personal_context_builder/wenet_analysis.py
@@ -45,9 +45,9 @@ def compare_routines(
     """
     model_num = config.MAP_MODEL_TO_DB[model]
     if is_mock:
-        db = DatabaseProfileHandlerMock.get_instance(model_num)
+        db = DatabaseProfileHandlerMock.get_instance(db_index=model_num)
     else:
-        db = DatabaseProfileHandler.get_instance(model_num)
+        db = DatabaseProfileHandler.get_instance(db_index=model_num)
     source_routine = db.get_profile(source_user)
     routines = [db.get_profile(u) for u in users]
     routines_dist = [function(source_routine, r) for r in routines]
