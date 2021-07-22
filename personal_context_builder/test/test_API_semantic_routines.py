601f69acd4d36607921d1e2281cdad64507c3496
William DROZ <william.droz@idiap.ch>
2021-07-22 11:26:42 +0200

    add test for semantic routines

---
+++ personal_context_builder/test/test_API_semantic_routines.py
@@ -0,0 +1,24 @@
+""" Test for the API that touch the routines
+
+Copyright (c) 2021 Idiap Research Institute, https://www.idiap.ch/
+Written by William Droz <william.droz@idiap.ch>,
+"""
+import unittest
+from uuid import uuid4
+
+from personal_context_builder import config
+from personal_context_builder.wenet_cli_entrypoint import train, update
+from personal_context_builder.wenet_fastapi_app import app
+
+from fastapi.testclient import TestClient
+
+
+class APISemanticRoutinesTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_confidence_exist(self):
+        response = self.client.get(
+            config.PCB_VIRTUAL_HOST_LOCATION + "/semantic_routines/mock_user_1/2/1700/"
+        )
+        self.assertIn("confidence", response.json())
