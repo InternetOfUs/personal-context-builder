5b6f66b65f9a685d252559afd5551c87d2a0df5f
William DROZ <william.droz@idiap.ch>
2021-10-29 16:48:56 +0200

    use base conda for coverage

--- docker_run_tests.sh
+++ docker_run_tests.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-docker run -v wenet-data:/data -e PCB_DATA_FOLDER=/data -e PCB_REDIS_HOST=wenet-redis docker.idiap.ch/wenet/personal_context_builder conda run -n wenet_fastapi ./run_tests.sh $@
+docker run -v wenet-data:/data -e PCB_DATA_FOLDER=/data -e PCB_REDIS_HOST=wenet-redis docker.idiap.ch/wenet/personal_context_builder ./run_tests.sh $@
