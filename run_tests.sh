c543f9b1b692e05dd3e59a56c71a7c93180c89ce
William DROZ <william.droz@idiap.ch>
2021-11-08 09:08:38 +0100

    remove skiping tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,2 @@
 #! /bin/bash
-if [ "$HOSTNAME" != "redbull" ]
-then
-    PCB_IS_UNITTESTING=1 coverage run --source=personal_context_builder -m unittest discover -s personal_context_builder && coverage report
-else
-    echo "Warning - Test skipped on this computer"
-fi
+PCB_IS_UNITTESTING=1 coverage run --source=personal_context_builder -m unittest discover -s personal_context_builder && coverage report
