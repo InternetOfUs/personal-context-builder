218fa766cc7a7fab08c72595764c50506ee5b421
William DROZ <william.droz@idiap.ch>
2022-05-11 08:55:52 +0200

    bump to v2.3.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt", "r") as f:
 
 setuptools.setup(
     name="personal_context_builder",
-    version="2.3.0",
+    version="2.3.1",
     license="Apache-2.0",
     author="Idiap - William Droz",
     author_email="william.droz@idiap.ch",
